d13e29170af08b16c7dc2ade19c320dcd712cf38
Ccel85 <ccelena90@gmail.com>
2026-02-19 21:05:36 +0100

    Fix index.php execution order

--- public/index.php
+++ public/index.php
@@ -2,9 +2,10 @@
 
 use App\Kernel;
 
+date_default_timezone_set('Europe/Paris');
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-date_default_timezone_set('Europe/Paris');
